8df3dd4d32d55f93c574aa7d15c2368e9db936c7
icres <icress26@gmail.com>
2023-02-27 14:52:03 -0700

    Updated img path and disappearing register link

--- main.py
+++ main.py
@@ -199,7 +199,8 @@ def post_category(category):
     elif category == 'Tech':
         category_name = 'Technology'
     category_pics = {
-        'Random Thoughts': '',
+        'Random Thoughts': 'https://images.unsplash.com/photo-1495567720989-cebdbdd97913?ixlib=rb-4.0.3&ixid=MnwxMjA3f'
+                           'DB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
         'Entertainment': 'https://images.unsplash.com/photo-1513346940221-6f673d962e97?ixlib=rb-4.0.3&'
                          'ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
         'Technology': 'https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MH'
